e8ab7f87570107b8524a21312b81831aa2a14fbb
David Scott <david.scott@datasoft.com>
2011-10-24 14:09:20 -0700

    Where applicable $HOME was used to replace certain paths in the .pro file, enough that it shouldn't need to be modified across developers/machines.  The paths to user/lib/ libraries and linking to the Qmake bins are still absolute. See ticket #84

--- NovaGUI/ui_novagui.h
+++ NovaGUI/ui_novagui.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'novagui.ui'
 **
-** Created: Mon Oct 24 10:40:01 2011
+** Created: Mon Oct 24 13:09:51 2011
 **      by: Qt User Interface Compiler version 4.7.4
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
