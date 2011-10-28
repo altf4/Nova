e68489a4a68965975332bc394341dde8407958b8
David Scott <dave.scott@datasoft.com>
2011-10-28 12:32:00 -0700

    Complete look and feel of configuration GUI
    Completed the base look and feel of the configuration window, no
    functionality implemented yet. See ticket #110

--- NovaGUI/ui_novagui.h
+++ NovaGUI/ui_novagui.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'novagui.ui'
 **
-** Created: Fri Oct 28 09:38:50 2011
+** Created: Fri Oct 28 11:57:04 2011
 **      by: Qt User Interface Compiler version 4.7.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
