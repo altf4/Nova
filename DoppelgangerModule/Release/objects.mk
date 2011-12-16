3290841cde54e32092fd0970e1bccc81f84ed352
Dan Petro <dan.petro@datasoft.com>
2011-12-16 16:29:05 -0700

    Changed project configuration settings to handle both Debug and Release modes
    - Changed Debug mode to output debugging symbols and not optimize
    - Added a Release mode that does optimize (-O3) and ommits debug symbols

---
+++ DoppelgangerModule/Release/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lNovaLibrary -llog4cxx -lboost_serialization -lann
+
