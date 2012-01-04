6ffe7ec265c1ec8bf72cb2feed484e64bd9aca93
David Clark <david.clark@datasoft.com>
2012-01-04 14:31:08 -0700

    Refactored to fix issues found in code review:
    	Changed from map to google dense hash map.
    	Fixed brace/formatting style.
    	Fixed a bug where variables left out of the config file will cause a crash when being accessed rather than returning an option with the invalid flag set.

--- NovaLibrary/src/NOVAConfiguration.cpp
+++ NovaLibrary/src/NOVAConfiguration.cpp
@@ -1,66 +1,88 @@
-/*
- * NOVAConfiguration.cpp
- *
- *  Created on: Dec 22, 2011
- *      Author: root
- */
+//============================================================================
+// Name        : NOVAConfiguration.h
+// Author      : DataSoft Corporation
+// Copyright   : GNU GPL v3
+// Description : Loads and parses the configuration file
+//============================================================================/*
 
 #include "NOVAConfiguration.h"
+#include <fstream>
+#include <iostream>
+#include <string.h>
 
-namespace Nova {
 
-NOVAConfiguration::NOVAConfiguration() {
+namespace Nova
+{
 
-}
-
-void NOVAConfiguration::LoadConfig(char* input, string homePath) {
+// Loads the configuration file into the class's state data
+void NOVAConfiguration::LoadConfig(char* input, string homePath)
+{
 	string line;
 	string prefix;
+	int prefixIndex;
 
 	cout << "Loading file " << input << " in homepath " << homePath << endl;
 
 	ifstream config(input);
 
-	const string prefixes[] = { "INTERFACE", "HS_HONEYD_CONFIG", "TCP_TIMEOUT",
-			"TCP_CHECK_FREQ", "READ_PCAP", "PCAP_FILE", "GO_TO_LIVE",
-			"USE_TERMINALS", "CLASSIFICATION_TIMEOUT", "BROADCAST_ADDR",
-			"SILENT_ALARM_PORT", "K", "EPS", "IS_TRAINING",
-			"CLASSIFICATION_THRESHOLD", "DATAFILE", "SA_MAX_ATTEMPTS",
-			"SA_SLEEP_DURATION", "DM_HONEYD_CONFIG",
-			"DOPPELGANGER_IP", "DM_ENABLED"};
+	const string prefixes[] =
+	{ "INTERFACE", "HS_HONEYD_CONFIG", "TCP_TIMEOUT", "TCP_CHECK_FREQ",
+			"READ_PCAP", "PCAP_FILE", "GO_TO_LIVE", "USE_TERMINALS",
+			"CLASSIFICATION_TIMEOUT", "BROADCAST_ADDR", "SILENT_ALARM_PORT",
+			"K", "EPS", "IS_TRAINING", "CLASSIFICATION_THRESHOLD", "DATAFILE",
+			"SA_MAX_ATTEMPTS", "SA_SLEEP_DURATION", "DM_HONEYD_CONFIG",
+			"DOPPELGANGER_IP", "DM_ENABLED" };
 
-	if (config.is_open()) {
-		while (config.good()) {
+	// Populate the options map
+	for (uint i = 0; i < sizeof(prefixes)/sizeof(prefixes[0]); i++)
+	{
+		NovaOption * currentOption = new NovaOption();
+		currentOption->isValid = false;
+		options[prefixes[i]] = *currentOption;
+	}
+
+	if (config.is_open())
+	{
+		while (config.good())
+		{
 			getline(config, line);
-			prefix = prefixes[0];
+			prefixIndex = 0;
+			prefix = prefixes[prefixIndex];
 
-			NovaOption * currentOption = new NovaOption();
-			currentOption->isValid = false;
 
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
+			// INTERFACE
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
 				line = line.substr(prefix.size() + 1, line.size());
-				if (line.size() > 0) {
+				if (line.size() > 0)
+				{
 					// Try and detect a default interface by checking what the default route in the IP kernel's IP table is
-					if (strcmp(line.c_str(), "default") == 0) {
+					if (strcmp(line.c_str(), "default") == 0)
+					{
 
 						FILE * out = popen("netstat -rn", "r");
-						if (out != NULL) {
+						if (out != NULL)
+						{
 							char buffer[2048];
 							char * column;
 							int currentColumn;
 							char * line = fgets(buffer, sizeof(buffer), out);
 
-							while (line != NULL) {
+							while (line != NULL)
+							{
 								currentColumn = 0;
 								column = strtok(line, " \t\n");
 
 								// Wait until we have the default route entry, ignore other entries
-								if (strcmp(column, "0.0.0.0") == 0) {
-									while (column != NULL) {
+								if (strcmp(column, "0.0.0.0") == 0)
+								{
+									while (column != NULL)
+									{
 										// Get the column that has the interface name
-										if (currentColumn == 7) {
+										if (currentColumn == 7)
+										{
 
-											currentOption->data = column;
+											options[prefix].data = column;
 											// LOG4CXX_INFO(m_logger, "Interface Device defaulted to '" + dev + "'");
 										}
 
@@ -74,271 +96,316 @@ void NOVAConfiguration::LoadConfig(char* input, string homePath) {
 							}
 						}
 						pclose(out);
-					} else
-						currentOption->data = line;
+					}
+					else
+						options[prefix].data = line;
 
-					if (!currentOption->data.empty())
-						currentOption->isValid = true;
+					if (!options[prefix].data.empty())
+						options[prefix].isValid = true;
 				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
-				continue;
-			}
-			prefix = prefixes[1];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
-				line = line.substr(prefix.size() + 1, line.size());
-				if (line.size() > 0) {
-					currentOption->data = homePath + "/" + line;
-					currentOption->isValid = true;
-				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
-				continue;
-			}
-			prefix = prefixes[2];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
-				line = line.substr(prefix.size() + 1, line.size());
-				if (atoi(line.c_str()) > 0) {
-					currentOption->data = line.c_str();
-					currentOption->isValid = true;
-				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
-				continue;
-			}
-			prefix = prefixes[3];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
-				line = line.substr(prefix.size() + 1, line.size());
-				if (atoi(line.c_str()) > 0) {
-					currentOption->data = line.c_str();
-					currentOption->isValid = true;
-				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
-				continue;
-			}
-			prefix = prefixes[4];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
-				line = line.substr(prefix.size() + 1, line.size());
-				if (atoi(line.c_str()) == 0 || atoi(line.c_str()) == 1) {
-					currentOption->data = line.c_str();
-					currentOption->isValid = true;
-				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
-				continue;
-			}
-			prefix = prefixes[5];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
-				line = line.substr(prefix.size() + 1, line.size());
-				if (line.size() > 0) {
-					currentOption->data = homePath + "/" + line;
-					currentOption->isValid = true;
-				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
 				continue;
 			}
 
-			prefix = prefixes[6];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
+			// HS_HONEYD_CONFIG
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
 				line = line.substr(prefix.size() + 1, line.size());
-				if (line.size() > 0) {
-					currentOption->data = line.c_str();
-					currentOption->isValid = true;
+				if (line.size() > 0)
+				{
+					options[prefix].data = homePath + "/" + line;
+					options[prefix].isValid = true;
 				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
 				continue;
 			}
 
-			prefix = prefixes[7];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
+			// TCP_TIMEOUT
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
 				line = line.substr(prefix.size() + 1, line.size());
-				if (atoi(line.c_str()) == 0 || atoi(line.c_str()) == 1) {
-					currentOption->data = line.c_str();
-					currentOption->isValid = true;
+				if (atoi(line.c_str()) > 0)
+				{
+					options[prefix].data = line.c_str();
+					options[prefix].isValid = true;
 				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
 				continue;
 			}
 
-			prefix = prefixes[8];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
+			// TCP_CHECK_FREQ
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
 				line = line.substr(prefix.size() + 1, line.size());
-				if (atoi(line.c_str()) > 0) {
-					currentOption->data = line.c_str();
-					currentOption->isValid = true;
+				if (atoi(line.c_str()) > 0)
+				{
+					options[prefix].data = line.c_str();
+					options[prefix].isValid = true;
 				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
 				continue;
 			}
 
-			// "BROADCAST_ADDR"
-			prefix = prefixes[9];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
+			// READ_PCAP
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
 				line = line.substr(prefix.size() + 1, line.size());
-				if (line.size() > 6 && line.size() < 16) {
-					currentOption->data = line.c_str();
-					currentOption->isValid = true;
+				if (atoi(line.c_str()) == 0 || atoi(line.c_str()) == 1)
+				{
+					options[prefix].data = line.c_str();
+					options[prefix].isValid = true;
 				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
 				continue;
 			}
 
-			// "SILENT_ALARM_PORT",
-			prefix = prefixes[10];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
+			// PCAP_FILE
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
 				line = line.substr(prefix.size() + 1, line.size());
-				if (atoi(line.c_str()) > 0) {
-					currentOption->data = line.c_str();
-					currentOption->isValid = true;
+				if (line.size() > 0)
+				{
+					options[prefix].data = homePath + "/" + line;
+					options[prefix].isValid = true;
 				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
 				continue;
 			}
 
-			// "K"
-			prefix = prefixes[11];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
+			// GO_TO_LIVE
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
 				line = line.substr(prefix.size() + 1, line.size());
-				if (atoi(line.c_str()) > 0) {
-					currentOption->data = line.c_str();
-					currentOption->isValid = true;
+				if (line.size() > 0)
+				{
+					options[prefix].data = line.c_str();
+					options[prefix].isValid = true;
 				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
 				continue;
 			}
 
-			//" EPS"
-			prefix = prefixes[12];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
+			// USE_TERMINALS
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
 				line = line.substr(prefix.size() + 1, line.size());
-				if (atof(line.c_str()) >= 0) {
-					currentOption->data = line.c_str();
-					currentOption->isValid = true;
+				if (atoi(line.c_str()) == 0 || atoi(line.c_str()) == 1)
+				{
+					options[prefix].data = line.c_str();
+					options[prefix].isValid = true;
 				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
 				continue;
 			}
-			// "IS_TRAINING"
-			prefix = prefixes[13];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
+
+			// CLASSIFICATION_TIMEOUT
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
 				line = line.substr(prefix.size() + 1, line.size());
-				if (atoi(line.c_str()) == 0 || atoi(line.c_str()) == 1) {
-					currentOption->data = line.c_str();
-					currentOption->isValid = true;
+				if (atoi(line.c_str()) > 0)
+				{
+					options[prefix].data = line.c_str();
+					options[prefix].isValid = true;
 				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
 				continue;
 			}
-			// "CLASSIFICATION_THRESHOLD"
-			prefix = prefixes[14];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
+
+			// BROADCAST_ADDR
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
 				line = line.substr(prefix.size() + 1, line.size());
-				if (atof(line.c_str()) >= 0) {
-					currentOption->data = line.c_str();
-					currentOption->isValid = true;
+				if (line.size() > 6 && line.size() < 16)
+				{
+					options[prefix].data = line.c_str();
+					options[prefix].isValid = true;
 				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
 				continue;
 			}
-			// "DATAFILE",
-			prefix = prefixes[15];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
+
+			// SILENT_ALARM_PORT
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
+				line = line.substr(prefix.size() + 1, line.size());
+				if (atoi(line.c_str()) > 0)
+				{
+					options[prefix].data = line.c_str();
+					options[prefix].isValid = true;
+				}
+				continue;
+			}
+
+			// K
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
+				line = line.substr(prefix.size() + 1, line.size());
+				if (atoi(line.c_str()) > 0)
+				{
+					options[prefix].data = line.c_str();
+					options[prefix].isValid = true;
+				}
+				continue;
+			}
+
+			// EPS
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
+				line = line.substr(prefix.size() + 1, line.size());
+				if (atof(line.c_str()) >= 0)
+				{
+					options[prefix].data = line.c_str();
+					options[prefix].isValid = true;
+				}
+				continue;
+			}
+
+			// IS_TRAINING
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
+				line = line.substr(prefix.size() + 1, line.size());
+				if (atoi(line.c_str()) == 0 || atoi(line.c_str()) == 1)
+				{
+					options[prefix].data = line.c_str();
+					options[prefix].isValid = true;
+				}
+				continue;
+			}
+
+			// CLASSIFICATION_THRESHOLD
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
+				line = line.substr(prefix.size() + 1, line.size());
+				if (atof(line.c_str()) >= 0)
+				{
+					options[prefix].data = line.c_str();
+					options[prefix].isValid = true;
+				}
+				continue;
+			}
+
+			// DATAFILE
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
 				line = line.substr(prefix.size() + 1, line.size());
 				if (line.size() > 0 && !line.substr(line.size() - 4,
-						line.size()).compare(".txt")) {
-					currentOption->data = line.c_str();
-					currentOption->isValid = true;
+						line.size()).compare(".txt"))
+				{
+					options[prefix].data = line.c_str();
+					options[prefix].isValid = true;
 				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
-				continue;
-			}
-			// "SA_MAX_ATTEMPTS"
-			prefix = prefixes[16];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
-				line = line.substr(prefix.size() + 1, line.size());
-				if (atoi(line.c_str()) > 0) {
-					currentOption->data = line.c_str();
-					currentOption->isValid = true;
-				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
-				continue;
-			}
-			//"SA_SLEEP_DURATION"
-			prefix = prefixes[17];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
-				line = line.substr(prefix.size() + 1, line.size());
-				if (atof(line.c_str()) >= 0) {
-					currentOption->data = line.c_str();
-					currentOption->isValid = true;
-				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
 				continue;
 			}
 
-			 //"DM_HONEYD_CONFIG"
-			prefix = prefixes[18];
-			if(!line.substr(0,prefix.size()).compare(prefix))
+			// SA_MAX_ATTEMPTS
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
 			{
-				line = line.substr(prefix.size()+1,line.size());
-				if(line.size() > 0)
+				line = line.substr(prefix.size() + 1, line.size());
+				if (atoi(line.c_str()) > 0)
 				{
-					currentOption->data = homePath + "/" + line;
-					currentOption->isValid = true;
+					options[prefix].data = line.c_str();
+					options[prefix].isValid = true;
 				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
 				continue;
 			}
 
-			// "DOPPELGANGER_IP"
-			prefix = prefixes[19];
-			if (!line.substr(0, prefix.size()).compare(prefix)) {
+			// SA_SLEEP_DURATION
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
 				line = line.substr(prefix.size() + 1, line.size());
-				if(line.size() > 0) {
-					currentOption->data = line.c_str();
-					currentOption->isValid = true;
+				if (atof(line.c_str()) >= 0)
+				{
+					options[prefix].data = line.c_str();
+					options[prefix].isValid = true;
 				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
 				continue;
 			}
-			// "DM_ENABLED"
-			prefix = prefixes[20];
-			if(!line.substr(0,prefix.size()).compare(prefix))
+
+			// DM_HONEYD_CONFIG
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
 			{
-				line = line.substr(prefix.size()+1,line.size());
-				if(atoi(line.c_str()) == 0 || atoi(line.c_str()) == 1)
+				line = line.substr(prefix.size() + 1, line.size());
+				if (line.size() > 0)
 				{
-					currentOption->data = line.c_str();
-					currentOption->isValid = true;
+					options[prefix].data = homePath + "/" + line;
+					options[prefix].isValid = true;
 				}
-				options.insert(
-						pair<string, NovaOption> (prefix, *currentOption));
+				continue;
+			}
+
+			// DOPPELGANGER_IP
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
+				line = line.substr(prefix.size() + 1, line.size());
+				if (line.size() > 0)
+				{
+					options[prefix].data = line.c_str();
+					options[prefix].isValid = true;
+				}
+				continue;
+			}
+
+			// DM_ENABLED
+			prefixIndex++;
+			prefix = prefixes[prefixIndex];
+			if (!line.substr(0, prefix.size()).compare(prefix))
+			{
+				line = line.substr(prefix.size() + 1, line.size());
+				if (atoi(line.c_str()) == 0 || atoi(line.c_str()) == 1)
+				{
+					options[prefix].data = line.c_str();
+					options[prefix].isValid = true;
+				}
 				continue;
 
 			}
 
 		}
-	} else {
+	}
+	else
+	{
+		// TODO: Change this to use the logger. Need to figure out the home path
+		// to get the logger set up, so putting this off until the that's moved to a utility class.
+		cout << "No configuration file found" << endl;
 		//LOG4CXX_INFO(m_logger, "No configuration file detected.");
 	}
 }
 
-NOVAConfiguration::~NOVAConfiguration() {
+NOVAConfiguration::NOVAConfiguration()
+{
+	options.set_empty_key("0");
+}
+
+NOVAConfiguration::~NOVAConfiguration()
+{
 }
 
 }
