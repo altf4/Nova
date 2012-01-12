1712cbd8e9a0c9d8c51958086e2c7ab6f7a67c7d
Addison Waldow <addison.waldow@datasoft.com>
2012-01-12 08:46:14 -0700

    Housekeeping stuff
    - Committing an updated version of the subdir.mk inside the NovaLibrary
       Release folder.

--- NovaLibrary/Release/src/subdir.mk
+++ NovaLibrary/Release/src/subdir.mk
@@ -6,23 +6,26 @@
 CPP_SRCS += \
 ../src/FeatureSet.cpp \
 ../src/GUIMsg.cpp \
+../src/NOVAConfiguration.cpp \
+../src/NovaUtil.cpp \
 ../src/Point.cpp \
-../src/Suspect.cpp \
-../src/TrafficEvent.cpp 
+../src/Suspect.cpp 
 
 OBJS += \
 ./src/FeatureSet.o \
 ./src/GUIMsg.o \
+./src/NOVAConfiguration.o \
+./src/NovaUtil.o \
 ./src/Point.o \
-./src/Suspect.o \
-./src/TrafficEvent.o 
+./src/Suspect.o 
 
 CPP_DEPS += \
 ./src/FeatureSet.d \
 ./src/GUIMsg.d \
+./src/NOVAConfiguration.d \
+./src/NovaUtil.d \
 ./src/Point.d \
-./src/Suspect.d \
-./src/TrafficEvent.d 
+./src/Suspect.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
