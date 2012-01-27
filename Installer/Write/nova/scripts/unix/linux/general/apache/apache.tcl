e23507fdc2647731e46f0fd4da739433be8b8a87
David Clark <david.clark@datasoft.com>
2012-01-26 18:19:14 -0700

    Clean up if statements/regexps into switches
    Added some preprocessing on the request string to fail earlier on bad requests

--- Installer/Write/nova/scripts/unix/linux/general/apache/apache.tcl
+++ Installer/Write/nova/scripts/unix/linux/general/apache/apache.tcl
@@ -13,12 +13,7 @@ set destinationPort [lindex $argv 3]
 
 # Stuff we might want to change into arguments
 set versionString "Apache/2.2.20 (Ubuntu)"
-
 set date [exec date]
-
-set getRequest ""
-set method ""
-set uri ""
 
 proc readFile {fileName} {
 	set fh [open $fileName "r"]
@@ -32,9 +27,8 @@ proc processOutputString {outputString} {
 	regsub {%DATE%} $outputString $::date outputString
 	regsub {%SERVER%} $outputString $::destinationIp outputString
 	regsub {%PORT%} $outputString $::destinationPort outputString
-	regsub {%REQUEST%} $outputString $::getRequest outputString
-	regsub {%METHOD%} $outputString $::method outputString
-	regsub {%URI%} $outputString $::uri outputString
+	regsub {%METHOD%} $outputString $::requestMethod outputString
+	regsub {%URI%} $outputString $::requestURI outputString
 	regsub {%SERVER_VERSION%} $outputString $::versionString outputString
 
 	puts -nonewline $outputString
@@ -42,11 +36,18 @@ proc processOutputString {outputString} {
 
 gets stdin request
 
-if {![regexp {^([^ ]+) ([^ ]+) ([^ ]+)} $request -> requestMethod requestURI requestVersion]} {
+if {![regexp {^([^ ]+) ([^ ]+) ?([^ ]*)} $request -> requestMethod requestURI requestVersion]} {
 	set outputString [readFile [file join $responseFolder 400]]
 	processOutputString $outputString
 	exit
 }
+
+if {$requestVersion == "" && $requestMethod != "GET" && $requestMethod != "POST"} {
+	set outputString [readFile [file join $responseFolder 400]]
+	processOutputString $outputString
+	exit
+}
+
 
 set line "bogusLine"
 while {$line != ""} {
@@ -54,35 +55,47 @@ while {$line != ""} {
 }
 
 
-# GET on the index
-if {[regexp {GET (/|/ .*|/index.html(\?.*)?|/index.html .*)$} $request]} {
-	set outputString [readFile [file join $responseFolder 200_index.header]]
-	append outputString [readFile [file join $responseFolder index.html]]
-# GET on a different file
-} elseif {[regexp {GET (/.*).*} $request -> getRequest]} {
-	set outputString [readFile [file join $responseFolder 404.header]]
-	append outputString [readFile [file join $responseFolder 404.html]]
-# HEAD on the index
-} elseif {[regexp {HEAD (/ .*|/index.html(\?.*)?|/index.html .*)$} $request]} {
-	set outputString [readFile [file join $responseFolder 200_index.header]]
-# HEAD on a different file
-} elseif {[regexp {HEAD (/.*).*} $request]} {
-	set outputString [readFile [file join $responseFolder 404.header]]
-# POST on index or some version of it
-} elseif {[regexp {POST (/|/ .*|/index.html(\?.*)?|/index.html .*)$} $request]} {
-	set outputString [readFile [file join $responseFolder 200_index.header]]
-# POST on any other file
-} elseif {[regexp {POST (/.*).*} $request]} {
-	set outputString [readFile [file join $responseFolder 404.header]]
-# OPTIONS command
-} elseif {[regexp {OPTIONS /[^ ]* [^ ]+} $request]} {
-	set outputString [readFile [file join $responseFolder 200_options.header]]
-# Undefined method
-} elseif {[regexp {([^ ]+) (/[^ ]*).*} $request -> method uri]} {
-	set outputString [readFile [file join $responseFolder 501]]
-# Who knows. Throw a 400 Invalid Request and hope it's a reasonble response to whatever they sent
-} else {
-	set outputString [readFile [file join $responseFolder 400]]
+switch -- $requestMethod {
+	GET {
+		if {[regexp {^/$|^/index.html(\?.*)?$} $requestURI]} {
+			set outputString [readFile [file join $responseFolder 200_index.header]]
+			append outputString [readFile [file join $responseFolder index.html]]
+		} else {
+			set outputString [readFile [file join $responseFolder 404.header]]
+			append outputString [readFile [file join $responseFolder 404.html]]
+		}
+	}
+
+	HEAD {
+		if {[regexp {^/$|^/index.html(\?.*)?$} $requestURI]} {
+			set outputString [readFile [file join $responseFolder 200_index.header]]
+		} else {
+			set outputString [readFile [file join $responseFolder 404.header]]
+		}
+
+	}
+
+	POST {
+		if {[regexp {^/$|^/index.html(\?.*)?$} $requestURI]} {
+			set outputString [readFile [file join $responseFolder 200_index.header]]
+			append outputString [readFile [file join $responseFolder index.html]]
+		} else {
+			set outputString [readFile [file join $responseFolder 404.header]]
+			append outputString [readFile [file join $responseFolder 404.html]]
+		}
+	}
+
+	OPTIONS {
+		if {[regexp {^\*$|^/.*} $requestURI]} {
+			set outputString [readFile [file join $responseFolder 200_options.header]]
+		} else {
+			set outputString [readFile [file join $responseFolder 400]]
+		}
+	}
+
+	default {
+		set outputString [readFile [file join $responseFolder 501]]
+	}
 }
 
 processOutputString $outputString
