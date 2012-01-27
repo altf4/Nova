459fc016f88b5f9945a756108206cc4136bd7bc0
David Clark <david.clark@datasoft.com>
2012-01-26 17:16:05 -0700

    Add a server_version var

--- Installer/Write/nova/scripts/unix/linux/general/apache/apache.tcl
+++ Installer/Write/nova/scripts/unix/linux/general/apache/apache.tcl
@@ -1,5 +1,8 @@
 #!/usr/bin/tclsh
 
+# TODO
+#
+
 set scriptsFolder "/usr/share/nova/scripts"
 set responseFolder [file join $scriptsFolder unix linux general apache responses]
 
@@ -7,6 +10,10 @@ set sourceIp [lindex $argv 0]
 set sourcePort [lindex $argv 1]
 set destinationIp [lindex $argv 2]
 set destinationPort [lindex $argv 3]
+
+# Stuff we might want to change into arguments
+set versionString "Apache/2.2.20 (Ubuntu)"
+
 set date [exec date]
 
 set getRequest ""
@@ -21,15 +28,34 @@ proc readFile {fileName} {
 	return $data
 }
 
+proc processOutputString {outputString} {
+	regsub {%DATE%} $outputString $::date outputString
+	regsub {%SERVER%} $outputString $::destinationIp outputString
+	regsub {%PORT%} $outputString $::destinationPort outputString
+	regsub {%REQUEST%} $outputString $::getRequest outputString
+	regsub {%METHOD%} $outputString $::method outputString
+	regsub {%URI%} $outputString $::uri outputString
+	regsub {%SERVER_VERSION%} $outputString $::versionString outputString
+
+	puts -nonewline $outputString
+}
+
 gets stdin request
+
+if {![regexp {^([^ ]+) ([^ ]+) ([^ ]+)} $request -> requestMethod requestURI requestVersion]} {
+	set outputString [readFile [file join $responseFolder 400]]
+	processOutputString $outputString
+	exit
+}
 
 set line "bogusLine"
 while {$line != ""} {
 	gets stdin line
 }
 
+
 # GET on the index
-if {[regexp {GET (/|/ .*|/index.html|/index.html .*)$} $request]} {
+if {[regexp {GET (/|/ .*|/index.html(\?.*)?|/index.html .*)$} $request]} {
 	set outputString [readFile [file join $responseFolder 200_index.header]]
 	append outputString [readFile [file join $responseFolder index.html]]
 # GET on a different file
@@ -37,11 +63,20 @@ if {[regexp {GET (/|/ .*|/index.html|/index.html .*)$} $request]} {
 	set outputString [readFile [file join $responseFolder 404.header]]
 	append outputString [readFile [file join $responseFolder 404.html]]
 # HEAD on the index
-} elseif {[regexp {HEAD (/|/index.html).*} $request]} {
+} elseif {[regexp {HEAD (/ .*|/index.html(\?.*)?|/index.html .*)$} $request]} {
 	set outputString [readFile [file join $responseFolder 200_index.header]]
 # HEAD on a different file
 } elseif {[regexp {HEAD (/.*).*} $request]} {
 	set outputString [readFile [file join $responseFolder 404.header]]
+# POST on index or some version of it
+} elseif {[regexp {POST (/|/ .*|/index.html(\?.*)?|/index.html .*)$} $request]} {
+	set outputString [readFile [file join $responseFolder 200_index.header]]
+# POST on any other file
+} elseif {[regexp {POST (/.*).*} $request]} {
+	set outputString [readFile [file join $responseFolder 404.header]]
+# OPTIONS command
+} elseif {[regexp {OPTIONS /[^ ]* [^ ]+} $request]} {
+	set outputString [readFile [file join $responseFolder 200_options.header]]
 # Undefined method
 } elseif {[regexp {([^ ]+) (/[^ ]*).*} $request -> method uri]} {
 	set outputString [readFile [file join $responseFolder 501]]
@@ -50,14 +85,6 @@ if {[regexp {GET (/|/ .*|/index.html|/index.html .*)$} $request]} {
 	set outputString [readFile [file join $responseFolder 400]]
 }
 
-regsub {%DATE%} $outputString $date outputString
-regsub {%SERVER%} $outputString $destinationIp outputString
-regsub {%PORT%} $outputString $destinationPort outputString
-regsub {%REQUEST%} $outputString $getRequest outputString
-regsub {%METHOD%} $outputString $method outputString
-regsub {%URI%} $outputString $uri outputString
-
-puts -nonewline $outputString
-
+processOutputString $outputString
 exit
 
