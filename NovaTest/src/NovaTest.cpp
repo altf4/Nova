a5058d0196be8a162ac54e2ec2fc5a34093618f0
David Clark <david.clark@datasoft.com>
2012-03-09 11:02:41 -0700

    Add suspectTable test

--- NovaTest/src/NovaTest.cpp
+++ NovaTest/src/NovaTest.cpp
@@ -4,9 +4,10 @@
 #include "tester_Config.h"
 #include "tester_FeatureSet.h"
 #include "tester_Suspect.h"
-#include "SuspectTable.h"
+#include "tester_SuspectTable.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
