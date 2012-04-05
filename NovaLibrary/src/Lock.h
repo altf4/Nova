b97547283d0e8c2163723371f5c3cf57b5d84f36
Dan Petro <dan.petro@datasoft.com>
2012-04-05 12:50:08 -0700

    Lib: Updated Lock.h to also include read/write locks

--- NovaLibrary/src/Lock.h
+++ NovaLibrary/src/Lock.h
@@ -30,21 +30,43 @@ class Lock
 public:
 	Lock(pthread_mutex_t *lock)
 	{
-		m_lock = lock;
-		pthread_mutex_lock(m_lock);
+		isMutex = true;
+		m_mutex = lock;
+		pthread_mutex_lock(m_mutex);
+	}
+
+	Lock(pthread_rwlock_t *lock, bool isReadLock)
+	{
+		isMutex = false;
+		m_rwlock = lock;
+		if(isReadLock)
+		{
+			pthread_rwlock_rdlock(m_rwlock);
+		}
+		else
+		{
+			pthread_rwlock_wrlock(m_rwlock);
+		}
 	}
 
 	~Lock()
 	{
-		pthread_mutex_unlock(m_lock);
+		if(isMutex)
+		{
+			pthread_mutex_unlock(m_mutex);
+		}
+		else
+		{
+			pthread_rwlock_unlock(m_rwlock);
+		}
 	}
 
 private:
-	pthread_mutex_t *m_lock;
-
+	bool isMutex;
+	pthread_mutex_t *m_mutex;
+	pthread_rwlock_t *m_rwlock;
 };
 
 }
-
 
 #endif /* LOCK_H_ */
