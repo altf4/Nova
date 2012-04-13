047e5f7f3bb2b85e4540759a9458e171be20f65c
David Clark <david.clark@datasoft.com>
2012-04-13 15:26:14 -0700

    UI_WEB: Fix web installation script
    Added nowjs package install call.
    Added links to dependencies.
    Removed configJS.cpp (not finished yet)
    Fixed symlink to www

--- NovaWeb/install.sh
+++ NovaWeb/install.sh
@@ -1,15 +1,57 @@
 #!/bin/bash
-
+#######################################################
 # Install helper script
-# Dependencies,
-#   Nodejs v0.6.12
-#   cvv8 headers in /usr/include
+#######################################################
 
-ln -f -T -s www /var/www
+
+#######################################################
+# Dependencies you must manually get first
+#######################################################
+#   Nodejs v0.6.12
+#	Currently at: https://github.com/joyent/node/zipball/v0.6.12
+#	Install with the standard ./configure && make && sudo make install
+#
+#######################################################
+#   cvv8 headers in /usr/include
+#	Currently at: http://v8-juice.googlecode.com/files/libv8-convert-20120219.tar.gz
+#	Install by copying the include/cvv8 folder to /usr/include/
+#
+#######################################################
+
+# Note: If you have another web server using /var/www,
+# you may need to change the path in tst.js or delete
+# what's there.
+echo "========================================"
+echo "Making symlink /var/www -> ~/Code/Nova/NovaWeb/www"
+echo "========================================"
+ln -f -T -s ~/Code/Nova/NovaWeb/www /var/www
+
+echo "========================================"
+echo "Making a symlink to our current Dojo version"
+echo "========================================"
 ln -f -T -s dojo-release-1.7.1 www/dojo
 
+echo "========================================"
+echo "Installing Nowjs"
+echo "========================================"
+npm install now -g 
+
+echo "========================================"
+echo "Configuring NOVA node"
+echo "========================================"
 node-waf configure
+
+# Compile the nova node
+# (This needs to be done every time the source changes)
+echo "========================================"
+echo "Building NOVA node"
+echo "========================================"
 node-waf
 
-# To run,
+# To run the Nova Web interface,
 # bash$ NODE_PATH=build/Release node tst.js
+
+echo "========================================"
+echo "Installation finished"
+echo "To run the web interface, run "NODE_PATH=build/Release node tst.js" from the NovaWeb folder"
+echo "========================================"
