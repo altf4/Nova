003575cb3aa896a56645f4ba5cb0828bc883b53a
David Clark <david.clark@datasoft.com>
2012-04-17 15:10:33 -0700

    UI_WEB: Make web interface use the generic setter/getter in Config class

--- NovaWeb/NodeNovaConfig/NovaConfigBinding.h
+++ NovaWeb/NodeNovaConfig/NovaConfigBinding.h
@@ -14,54 +14,9 @@ class NovaConfigBinding : public node::ObjectWrap {
 
   static v8::Handle<v8::Value> New(const v8::Arguments& args);
 
-static v8::Handle<v8::Value> GetClassificationThreshold(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetClassificationTimeout(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetConfigFilePath(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetDataTTL(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetDoppelInterface(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetDoppelIp(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetEnabledFeatures(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetEnabledFeatureCount(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetEps(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetGotoLive(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetInterface(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetIsDmEnabled(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetIsTraining(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetK(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetPathCESaveFile(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetPathConfigHoneydUser(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetPathConfigHoneydHS(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetPathPcapFile(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetPathTrainingCapFolder(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetPathTrainingFile(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetReadPcap(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetSaMaxAttempts(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetSaPort(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetSaSleepDuration(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetSaveFreq(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetTcpCheckFreq(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetTcpTimout(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetThinningDistance(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetKey(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetNeighbors(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetGroup(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetLoggerPreferences(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetSMTPAddr(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetSMTPDomain(const v8::Arguments& args);
-//static v8::Handle<v8::Value> GetSMTPEmailRecipients(const v8::Arguments& args);
-//static v8::Handle<v8::Value> GetSMTPPort(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetPathBinaries(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetPathWriteFolder(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetPathReadFolder(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetPathIcon(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetPathHome(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetSqurtEnabledFeatures(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetHaystackStorage(const v8::Arguments& args);
-static v8::Handle<v8::Value> GetUserPath(const v8::Arguments& args);
-
-
-
-  double counter_;
+  static v8::Handle<v8::Value> ReadSetting(const v8::Arguments& args);
+  static v8::Handle<v8::Value> WriteSetting(const v8::Arguments& args);
+  
   Nova::Config *m_conf;
 };
 
--- NovaWeb/tst.js
+++ NovaWeb/tst.js
@@ -1,20 +1,8 @@
 var novaNode = require('./build/Release/nova.node');
 var nova = new novaNode.Instance();
-console.log("STARTUP: Created a new instance of novaNode");
 
 var novaconfig = require('./NodeNovaConfig/build/Release/novaconfig');
 var config = new novaconfig.NovaConfigBinding();
-
-//console.info("Nova up: " + nova.IsNovadUp(true));
-//if( ! nova.IsNovadUp() )
-//{
-//    console.info("Start Novad: " + nova.StartNovad());
-//}
-
-//nova.OnNewSuspect(function(suspect)
-//{ 
-//    console.info("New suspect: " + suspect.GetInAddr());
-//});
 
 var fs = require('fs');
 var express=require('express');
@@ -45,195 +33,98 @@ app.configure(function () {
 console.info("Serving static GET at /var/www");
 app.use(express.static('/var/www'));
 
-app.post('/', function(req, res) {
-		console.info("GOT A POST REQUEST FOR CONFIGURATION! YAY!" + "interface was " + req.body.interface);	
-		});
+console.info("Listening on 8042");
+app.listen(8042);
+var everyone = require("now").initialize(app);
+		
+		
+// Nova config stuff		
+var configItems = [
+	"INTERFACE",
+	"HS_HONEYD_CONFIG",
+	"TCP_TIMEOUT",
+	"TCP_CHECK_FREQ",
+	"READ_PCAP",
+	"PCAP_FILE",
+	"GO_TO_LIVE",
+	"CLASSIFICATION_TIMEOUT",
+	"SILENT_ALARM_PORT",
+	"K",
+	"EPS",
+	"IS_TRAINING",
+	"CLASSIFICATION_THRESHOLD",
+	"DATAFILE",
+	"SA_MAX_ATTEMPTS",
+	"SA_SLEEP_DURATION",
+	"USER_HONEYD_CONFIG",
+	"DOPPELGANGER_IP",
+	"DOPPELGANGER_INTERFACE",
+	"DM_ENABLED",
+	"ENABLED_FEATURES",
+	"TRAINING_CAP_FOLDER",
+	"THINNING_DISTANCE",
+	"SAVE_FREQUENCY",
+	"DATA_TTL",
+	"CE_SAVE_FILE",
+	"SMTP_ADDR",
+	"SMTP_PORT",
+	"SMTP_DOMAIN",
+	"RECIPIENTS",
+	"SERVICE_PREFERENCES",
+	"HAYSTACK_STORAGE"
+];
 
-app.get('/configureNova', function(req, res) {
-	console.log("[200] " + req.method + " to " + req.url);
+
+app.post('/configureNovaSave', function(req, res) {
+	var result = true;
+	for (var item = 0; item < configItems.length; item++) {
+		if (req.body[configItems[item]] !== undefined) {
+			if (!config.WriteSetting(configItems[item], req.body[configItems[item]])) {
+				result = false;
+			}
+		}
+	}
+	
 	res.writeHead(200, "OK", {'Content-Type': 'text/html'});
-	res.write('<HTML>');
-	res.write('<HEAD>');
-	res.write(' <link rel="stylesheet" type="text/css" href="style.css" media="screen">');
-	res.write('</HEAD>');
-	res.write('<BODY>');
-
-	res.write('<H1> NOVA Configuration </H1>');
-	res.write('<form method="post" action="/">');
-	res.write('<label>Interface</label>');
-	res.write('<input type="text" name="interface" value="');
-	res.write(config.GetInterface());
-	res.write('" /><br />');
-
-	res.write('<label>Data File</label>');
-	res.write('<input type="text" name="datafile" value="');
-	res.write(config.GetPathTrainingFile().toString());
-	res.write('" /><br />');
-
-	res.write('<label>Silent Alarm Port</label>');
-	res.write('<input type="text" name="saport" value="');
-	res.write(config.GetSaPort().toString());
-	res.write('" /><br />');
-
-	res.write('<label>Silent Alarm Connection Attempts</label>');
-	res.write('<input type="text" name="saconnectattempts" value="');
-	res.write(config.GetSaMaxAttempts().toString());
-	res.write('" /><br />');
-
-	res.write('<label>K</label>');
-	res.write('<input type="text" name="k" value="');
-	res.write(config.GetK().toString());
-	res.write('" /><br />');
-
-	res.write('<label>EPS</label>');
-	res.write('<input type="text" name="EPS" value="');
-	res.write(config.GetEps().toString());
-	res.write('" /><br />');
-
-	res.write('<label>Classification Timeout</label>');
-	res.write('<input type="text" name="classification_timeout" value="');
-	res.write(config.GetClassificationTimeout().toString());
-	res.write('" /><br />');
-
-	res.write('<label>Training Capture Folder</label>');
-	res.write('<input type="text" name="training_cap_folder" value="');
-	res.write(config.GetPathTrainingCapFolder());
-	res.write('" /><br />');
-
-	res.write('<label>Thinning Distance</label>');
-	res.write('<input type="text" name="thinning_distance" value="');
-	res.write(config.GetThinningDistance().toString());
-	res.write('" /><br />');
-
-	res.write('<label>Classification Threshold</label>');
-	res.write('<input type="text" name="classification_threshold" value="');
-	res.write(config.GetClassificationThreshold().toString());
-	res.write('" /><br />');
-
-	res.write('<label>User Honeyd Config</label>');
-	res.write('<input type="text" name="user_honeyd_config" value="');
-	res.write(config.GetPathConfigHoneydUser().toString());
-	res.write('" /><br />');
-
-	res.write('<label>Doppelganger IP Address</label>');
-	res.write('<input type="text" name="Doppelganger_ip" value="');
-	res.write(config.GetDoppelIp().toString());
-	res.write('" /><br />');
-
-	res.write('<label>Doppelganger Interface</label>');
-	res.write('<input type="text" name="doppelganger_interface" value="');
-	res.write(config.GetDoppelInterface().toString());
-	res.write('" /><br />');
-
-	res.write('<label>Haystack Honeyd Config</label>');
-	res.write('<input type="text" name="hs_honeyd_config" value="');
-	res.write(config.GetPathConfigHoneydHS().toString());
-	res.write('" /><br />');
-
-	res.write('<label>TCP Timeout</label>');
-	res.write('<input type="text" name="tcp_timeout" value="');
-	res.write(config.GetPathTrainingFile().toString());
-	res.write('" /><br />');
-
-	res.write('<label>TCP Check Frequency</label>');
-	res.write('<input type="text" name="tcp_check_freq" value="');
-	res.write(config.GetTcpCheckFreq().toString());
-	res.write('" /><br />');
-
-	res.write('<label>Pcap file path</label>');
-	res.write('<input type="text" name="pcac_file" value="');
-	res.write(config.GetPathPcapFile().toString());
-	res.write('" /><br />');
-
-	res.write('<label>Silent Alarm Max connection attempts</label>');
-	res.write('<input type="text" name="sa_max_attempts" value="');
-	res.write(config.GetSaMaxAttempts().toString());
-	res.write('" /><br />');
-
-	res.write('<label>Silent Alarm Sleep Duration</label>');
-	res.write('<input type="text" name="sa_sleep_duration" value="');
-	res.write(config.GetSaSleepDuration().toString());
-	res.write('" /><br />');
-
-	res.write('<label>Enabled Featureset Mask</label>');
-	res.write('<input type="text" name="enabled_features" value="');
-	res.write(config.GetEnabledFeatures().toString());
-	res.write('" /><br />');
-
-	res.write('<label>Data TTL</label>');
-	res.write('<input type="text" name="data_ttl" value="');
-	res.write(config.GetDataTTL().toString());
-	res.write('" /><br />');
-
-	res.write('<label>State Save File</label>');
-	res.write('<input type="text" name="ce_save_file" value="');
-	res.write(config.GetPathCESaveFile().toString());
-	res.write('" /><br />');
-
-	res.write('<label>SMTP Address</label>');
-	res.write('<input type="text" name="smtp_addr" value="');
-	res.write(config.GetSMTPAddr().toString());
-	res.write('" /><br />');
-
-	res.write('<label>SMTP Domain</label>');
-	res.write('<input type="text" name="smtp_domain" value="');
-	res.write(config.GetSMTPDomain().toString());
-	res.write('" /><br />');
-
-	res.write('<label>Log Preferences</label>');
-	res.write('<input type="text" name="service_preferences" value="');
-	res.write(config.GetLoggerPreferences().toString());
-	res.write('" /><br />');
-
-	//res.write('<label>Haystack Storage</label>');
-	//res.write('<input type="text" name="haystack_storage" value="');
-	//res.write('" /><br />');
-
-	res.write('<label>Doppelganger Enabled?</label>');
-	if (config.GetIsDmEnabled()) {
-		res.write('<input type="checkbox" name="dm_enabled" value="" checked/><br />');
+	if (result) {
+		res.write("Settings saved! You will be redirected back to the settings page...");
 	} else {
-		res.write('<input type="checkbox" name="dm_enabled" value="" /><br />');
+		res.write("There was an error when attempting to write settings to configuration file");
 	}
-
-	res.write('<label>Training?</label>');
-	if (config.GetIsTraining()) {
-		res.write('<input type="checkbox" name="is_training" value="" checked/><br />');
-	} else {
-		res.write('<input type="checkbox" name="is_training" value="" /><br />');
-	}
-
-	res.write('<label>Read Pcap?</label>');
-	if (config.GetReadPcap()) {
-		res.write('<input type="checkbox" name="read_pcap" value="" checked/><br />');
-	} else {
-		res.write('<input type="checkbox" name="read_pcap" value="" /><br />');
-	}
-
-	res.write('<label>Go to live capture?</label>');
-	if (config.GetGotoLive()) {
-		res.write('<input type="checkbox" name="read_pcap" value="" checked/><br />');
-	} else {
-		res.write('<input type="checkbox" name="read_pcap" value="" /><br />');
-	}
-
-	res.write('<input type="submit" name="submitbutton" id="submitbutton" value="Save Settings" />');
-	res.write('</form>');
-	res.write('</BODY>');
-	res.write('</HTML>');
-
 	res.end();
 });
 
+app.get('/configureNova', function(req, res) {
+		console.log("[200] " + req.method + " to " + req.url);
+		res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+		res.write('<HTML>');
+		res.write('<HEAD>');
+		res.write(' <link rel="stylesheet" type="text/css" href="configstyle.css" media="screen">');
+		res.write('</HEAD>');
+		res.write('<BODY>');
 
+		res.write('<H1> NOVA Configuration </H1>');
 
-console.info("Listening on 8042");
-app.listen(8042);
+		for (var item = 0; item < configItems.length; item++) {
+			res.write('<label>');
+			res.write(configItems[item]);
+			res.write('</label>');
+			res.write('<form method="post" action="/configureNovaSave">');
+			res.write('<input type="text" name="');
+			res.write(configItems[item]);
+			res.write('" value="');
+			res.write(config.ReadSetting(configItems[item]));
+			res.write('" /><br />');
+		}
 
+		res.write('<input type="submit" name="submitbutton" id="submitbutton" value="Save Settings" />');
+		res.write('</form>');
+		res.write("<br/>");
+		res.write('</BODY>');
+		res.write('</HTML>');
 
-
-var everyone = require("now").initialize(app);
-
+		res.end();
+});
 
 
 
@@ -288,14 +179,12 @@ nova.registerOnNewSuspect(distributeSuspect);
 //console.log("Registered NewSuspect callback function.");
 
 
-process.on('SIGINT', function()
-		{
-		nova.Shutdown();
-		process.exit();
-		});
+process.on('SIGINT', function() {
+	nova.Shutdown();
+	process.exit();
+});
 
-function objCopy(src,dst)
-{
+function objCopy(src,dst) {
 	for (var member in src)
 	{
 		if( typeof src[member] == 'function' )
@@ -318,7 +207,7 @@ function objCopy(src,dst)
 setInterval(function() {
 		everyone.now.updateHaystackStatus(nova.IsHaystackUp());
 		everyone.now.updateNovadStatus(nova.IsNovadUp(false));
-		}, 5000);
+}, 5000);
 
 
 
