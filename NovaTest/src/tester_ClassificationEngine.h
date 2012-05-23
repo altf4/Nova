ca16f56500b55b8fc541c03f9d14f2850aa378b8
David Clark <david.clark@datasoft.com>
2012-05-22 17:54:15 -0700

    UTEST: Make a unit test to experiment with k fold cross validation
    See https://github.com/DataSoft/Nova/wiki/Classification-Research

--- NovaTest/src/tester_ClassificationEngine.h
+++ NovaTest/src/tester_ClassificationEngine.h
@@ -17,10 +17,13 @@
 //============================================================================/*
 
 #include "gtest/gtest.h"
+#include <iostream>
+#include <fstream>
 
 #include "ClassificationEngine.h"
 
 using namespace Nova;
+using namespace std;
 
 // The test fixture for testing class ClassificationEngine.
 class ClassificationEngineTest : public ::testing::Test
@@ -45,4 +48,133 @@ TEST_F(ClassificationEngineTest, test_someMethod)
 	Config::Inst()->SetIsDmEnabled(false);
 	EXPECT_EQ(0.42, ClassificationEngine::Normalize(LINEAR, 42, 0, 100));
 	Config::Inst()->SetIsDmEnabled(isDmEn);
+}
+
+
+TEST_F(ClassificationEngineTest, DISABLED_test_kFoldCrossValidation)
+{
+	chdir(Config::Inst()->GetPathHome().c_str());
+
+	vector<double*> trainingData;
+
+	ifstream trainingFile;
+	trainingFile.open(Config::Inst()->GetPathTrainingFile());
+
+	EXPECT_TRUE(trainingFile.is_open());
+
+	string line;
+	char *cLine;
+	char *token;
+	double *point;
+
+
+	while (getline(trainingFile, line)) {
+		cLine = (char*)line.c_str();
+		token = strtok(cLine, " ");
+
+		point = new double[DIM + 1];
+		int i = 0;
+		while (token != NULL)
+		{
+			point[i] = atof(token);
+
+			token = strtok (NULL, " ");
+			i++;
+		}
+		trainingData.push_back(point);
+	}
+
+
+	#define FOLDS 10
+
+
+	// Shuffle our data
+	srand ( time(NULL) );
+	for (uint i = 0; i < trainingData.size(); i++)
+	{
+		int r = rand()%trainingData.size();
+		double* tmp = trainingData.at(r);
+		trainingData.at(r) = trainingData.at(i);
+		trainingData.at(i) = tmp;
+	}
+
+
+	int takeAwayDim, takeAwayDim2;
+	char enableMask[DIM];
+	for (takeAwayDim = 0; takeAwayDim < DIM; takeAwayDim++)
+	{
+		for (takeAwayDim2 = takeAwayDim; takeAwayDim2 < DIM; takeAwayDim2++)
+		{
+			for (int d = 0; d < DIM; d++)
+				enableMask[d] = '0';
+
+			if (takeAwayDim == takeAwayDim2)
+				continue;
+
+			enableMask[takeAwayDim] = '1';
+			enableMask[takeAwayDim2] = '1';
+			Config::Inst()->SetEnabledFeatures(string(enableMask));
+
+
+
+			Suspect testSuspect;
+			FeatureSet fs;
+			double falsePositiveRatio = 0, falseNegativeRatio = 0, falseRatio = 0;
+			int falsePositives = 0, falseNegatives = 0;
+
+			for (uint i = 1; i <= FOLDS; i++)
+			{
+				int foldStart = (i - 1)*((double)trainingData.size()/(double)FOLDS);
+				int foldEnd = i*((double)trainingData.size()/(double)FOLDS) - 1;
+
+				vector<double*> subset = trainingData;
+				if (foldStart == foldEnd)
+				{
+					subset.erase(subset.begin() + foldStart);
+				}
+				else
+				{
+					subset.erase(subset.begin() + foldStart, subset.begin() + foldEnd);
+				}
+
+
+				testObject->LoadDataPointsFromVector(subset);
+
+				for (int point = foldStart; point <= foldEnd; point++)
+				{
+					for (int j = 0; j < DIM; j++)
+					{
+						fs.m_features[j] = trainingData.at(point)[j];
+					}
+
+					testSuspect.SetFeatureSet(&fs, MAIN_FEATURES);
+					testObject->Classify(&testSuspect);
+
+					if (testSuspect.GetIsHostile() && (trainingData.at(point)[DIM] == 0))
+					{
+						falsePositives++;
+					}
+
+					if (!testSuspect.GetIsHostile() && (trainingData.at(point)[DIM] == 1))
+					{
+						falseNegatives++;
+					}
+
+				}
+
+			}
+
+			falsePositiveRatio = (double)falsePositives / (double)trainingData.size();
+			falseNegativeRatio = (double)falseNegatives / (double)trainingData.size();
+			falseRatio = 1 - (double)(falseNegatives + falsePositives)/ (double)trainingData.size();
+
+			cout << "False positives: " << falsePositives << endl;
+			cout << "False positives: " << falsePositiveRatio*100 << "%" << endl;
+			cout << "False negatives: " << falseNegatives << endl;
+			cout << "False negatives: " << falseNegativeRatio*100 << "%" << endl;
+			cout << "Overall classification accuracy: " << falseRatio*100 << endl;
+			cout << FeatureSet::m_featureNames[takeAwayDim] << " + " << FeatureSet::m_featureNames[takeAwayDim2] << " " << falseRatio*100  << endl;
+			//cout << falseRatio*100  << endl;
+}
+	}
 }
