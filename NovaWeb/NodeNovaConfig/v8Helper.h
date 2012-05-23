e5dd4cb2be37b2527331a6e36735960b58bbaa5e
David Clark <david.clark@datasoft.com>
2012-05-23 15:56:13 -0700

    UI_WEB: Make a wrapper template for 2 arguement static methods

--- NovaWeb/NodeNovaConfig/v8Helper.h
+++ NovaWeb/NodeNovaConfig/v8Helper.h
@@ -54,6 +54,26 @@ v8::Handle<v8::Value> InvokeMethod(const v8::Arguments& args)
 }
 
 
+// Invocation of standalone (non-member) functions,
+// two-argument version version.
+template <typename NATIVE_RETURN, typename NATIVE_P1, typename NATIVE_P2, NATIVE_RETURN (*F)(NATIVE_P1, NATIVE_P2)> 
+v8::Handle<v8::Value> InvokeMethod(const v8::Arguments& args)
+{
+    using namespace v8;
+    HandleScope scope;
+
+    if( args.Length() < 2 )
+    {
+        return ThrowException(Exception::TypeError(String::New("Must be invoked with two parameters")));
+    }
+
+    NATIVE_P1 p1 = cvv8::CastFromJS<NATIVE_P1>( args[0] );
+    NATIVE_P2 p2 = cvv8::CastFromJS<NATIVE_P2>( args[0] );
+
+    Handle<v8::Value> result = cvv8::CastToJS(F(p1, p2));
+    return scope.Close(result);
+}
+
 
 
 
