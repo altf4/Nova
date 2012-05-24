905cc9b0da1e8812b892815971b213af514382a6
Addison Waldow <addison.waldow@datasoft.com>
2012-05-24 10:13:19 -0700

    UI_WEB: Finishing support for multiple interfaces
    All that needs doing now is the backend stuff in the C++ code and then I can
    integrate functionality for the app.post() for /configNova to select 
    more than one interface from the Web UI

--- NovaWeb/main.js
+++ NovaWeb/main.js
@@ -149,7 +149,8 @@ app.get('/configNova', ensureAuthenticated, function(req, res) {
      res.render('config.jade', 
 	 {
 		locals: {
-			INTERFACES: config.ReadSetting("INTERFACE").split(" ")
+			INTERFACES: config.ReadSetting("INTERFACE").split(" ") // will be a GetInterfaces() object in config object
+			,DEFAULT: false // should be a getDefault() method in Config object
 			,HS_HONEYD_CONFIG: config.ReadSetting("HS_HONEYD_CONFIG")
 			,TCP_TIMEOUT: config.ReadSetting("TCP_TIMEOUT")
 			,TCP_CHECK_FREQ: config.ReadSetting("TCP_CHECK_FREQ")
