4c3fe620352142e0ff251f571adcf9c4a91b36c7
David Scott <dave.scott@datasoft.com>
2012-06-07 17:03:00 -0700

    HSCONFIG_WIZARD: Include basic qt classes and project
    Basic placeholder project for future qt wizard for autoconfig tool

---
+++ HaystackConfigWizard/src/pages/NetworkSelectionPage.h
@@ -0,0 +1,27 @@
+/*
+ * HaystackConfigWizard.h
+ *
+ *  Created on: Jun 7, 2012
+ *      Author: victim
+ */
+
+#include <Qt>
+#include <QtGui>
+#include <QApplication>
+
+#ifndef NETWORKSELECTIONPAGE_H_
+#define NETWORKSELECTIONPAGE_H_
+
+class NetworkSelectionPage : public QWizardPage
+{
+	Q_OBJECT
+
+public:
+
+	NetworkSelectionPage(QWidget *parent = 0);
+
+private:
+
+};
+
+#endif /* NETWORKSELECTIONPAGE_H_ */
