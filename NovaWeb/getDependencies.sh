a17abe02efd90a96b09ff80717702f0d8072c4d2
David Clark <pherricoxide@miniserver.(none)>
2012-06-11 19:32:25 -0700

    UI_WEB/MISC: Add webui instructions to README
    Also updated the getDependencies.sh script to pull nodejs
    from source instead of the repos (was pulling wrong version).

--- NovaWeb/getDependencies.sh
+++ NovaWeb/getDependencies.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # This is a quick install script for nodejs and cvv8 headers
 
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install nodejs nodejs-dev npm
+# Get nodejs
+wget https://github.com/joyent/node/zipball/v0.6.12
+unzip v0.6.12
+cd joyent-node-346a3a8
+./configure
+make
+make install
 
+# Get cvv8
 wget http://v8-juice.googlecode.com/files/libv8-convert-20120219.tar.gz
 tar -xf libv8-convert-20120219.tar.gz
 sudo cp -fr libv8-convert-20120219/include/cvv8 /usr/include
