5a6128e92c576da98fe1a17a7321c5a7fea7c2b3
David Clark <david.clark@datasoft.com>
2012-06-14 15:40:35 -0700

    UI_WEB: Add a redirect to the basics settings page in the web interface

--- NovaWeb/main.js
+++ NovaWeb/main.js
@@ -155,45 +155,25 @@ app.get('/advancedOptions', ensureAuthenticated, function(req, res) {
 });
 
 
-app.get('/basicOptions', ensureAuthenticated, function(req, res) {
-     res.render('basicOptions.jade', 
+function renderBasicOptions(jadefile, res, req) {
+     res.render(jadefile, 
 	 {
 		locals: {
 			INTERFACES: config.ReadSetting("INTERFACE").split(" ") // will be a GetInterfaces() object in config object
 			,DEFAULT: false // should be a getDefault() method in Config object
-			,HS_HONEYD_CONFIG: config.ReadSetting("HS_HONEYD_CONFIG")
-			,TCP_TIMEOUT: config.ReadSetting("TCP_TIMEOUT")
-			,TCP_CHECK_FREQ: config.ReadSetting("TCP_CHECK_FREQ")
-			,READ_PCAP: config.ReadSetting("READ_PCAP")
-			,PCAP_FILE: config.ReadSetting("PCAP_FILE")
-			,GO_TO_LIVE: config.ReadSetting("GO_TO_LIVE")
-			,CLASSIFICATION_TIMEOUT: config.ReadSetting("CLASSIFICATION_TIMEOUT")
-			,SILENT_ALARM_PORT: config.ReadSetting("SILENT_ALARM_PORT")
-			,K: config.ReadSetting("K")
-			,EPS: config.ReadSetting("EPS")
-			,IS_TRAINING: config.ReadSetting("IS_TRAINING")
-			,CLASSIFICATION_THRESHOLD: config.ReadSetting("CLASSIFICATION_THRESHOLD")
-			,DATAFILE: config.ReadSetting("DATAFILE")
-			,SA_MAX_ATTEMPTS: config.ReadSetting("SA_MAX_ATTEMPTS")
-			,SA_SLEEP_DURATION: config.ReadSetting("SA_SLEEP_DURATION")
-			,USER_HONEYD_CONFIG: config.ReadSetting("USER_HONEYD_CONFIG")
 			,DOPPELGANGER_IP: config.ReadSetting("DOPPELGANGER_IP")
 			,DOPPELGANGER_INTERFACE: config.ReadSetting("DOPPELGANGER_INTERFACE")
 			,DM_ENABLED: config.ReadSetting("DM_ENABLED")
-			,ENABLED_FEATURES: config.ReadSetting("ENABLED_FEATURES")
-			,TRAINING_CAP_FOLDER: config.ReadSetting("TRAINING_CAP_FOLDER")
-			,THINNING_DISTANCE: config.ReadSetting("THINNING_DISTANCE")
-			,SAVE_FREQUENCY: config.ReadSetting("SAVE_FREQUENCY")
-			,DATA_TTL: config.ReadSetting("DATA_TTL")
-			,CE_SAVE_FILE: config.ReadSetting("CE_SAVE_FILE")
 			,SMTP_ADDR: config.ReadSetting("SMTP_ADDR")
 			,SMTP_PORT: config.ReadSetting("SMTP_PORT")
 			,SMTP_DOMAIN: config.ReadSetting("SMTP_DOMAIN")
 			,RECIPIENTS: config.ReadSetting("RECIPIENTS")
-			,SERVICE_PREFERENCES: config.ReadSetting("SERVICE_PREFERENCES")
-			,HAYSTACK_STORAGE: config.ReadSetting("HAYSTACK_STORAGE")
 		}
-	 })
+	 });
+}
+
+app.get('/basicOptions', ensureAuthenticated, function(req, res) {
+	renderBasicOptions('basicOptions.jade', res, req);
 });
 
 app.get('/configHoneydNodes', ensureAuthenticated, function(req, res) {
@@ -326,7 +306,7 @@ app.get('/novaMain', ensureAuthenticated, function(req, res) {
 app.get('/createNewUser', ensureAuthenticated, function(req, res) {res.render('createNewUser.jade');});
 app.get('/welcome', ensureAuthenticated, function(req, res) {res.render('welcome.jade');});
 app.get('/setup1', ensureAuthenticated, function(req, res) {res.render('setup1.jade');});
-app.get('/setup2', ensureAuthenticated, function(req, res) {res.render('setup2.jade');});
+app.get('/setup2', ensureAuthenticated, function(req, res) {renderBasicOptions('setup2.jade', res, req)});
 app.get('/setup3', ensureAuthenticated, function(req, res) {res.render('setup3.jade');});
 
 app.post('/createNewUser', ensureAuthenticated, function(req, res) {
@@ -372,7 +352,7 @@ app.post('/createInitialUser', ensureAuthenticated, function(req, res) {
 	    
         client.query('INSERT INTO ' + credTb + ' values(' + client.escape(userName) + ', SHA1(' + client.escape(password) + '))');
 		client.query('DELETE FROM ' + credTb + ' WHERE user = ' + client.escape('nova'));
-		res.render('saveRedirect.jade', { locals: {redirectLink: "'/'"}})	
+		res.render('saveRedirect.jade', { locals: {redirectLink: "'setup2'"}})	
         return;
       } 
       else
