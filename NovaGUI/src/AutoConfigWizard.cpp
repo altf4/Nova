d67b946d59c158c7d5bdd0088ee443b2e8264a17
David Scott <dave.scott@datasoft.com>
2012-06-26 11:31:01 -0700

    Qt: New class for Auto config wizard in qt project

---
+++ NovaGUI/src/AutoConfigWizard.cpp
@@ -0,0 +1,12 @@
+#include "AutoConfigWizard.h"
+
+AutoConfigWizard::AutoConfigWizard(QWidget *parent)
+    : QMainWindow(parent)
+{
+	ui.setupUi(this);
+}
+
+AutoConfigWizard::~AutoConfigWizard()
+{
+
+}
