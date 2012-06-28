52c211a1e969b9a613735ce208122cbb25b1bd52
David Scott <dave.scott@datasoft.com>
2012-06-28 15:38:28 -0700

    UI_Core: Revert HoneydConfiguration changes that broke ptrees
    In delete node there was a check for a default node that doesn't exist, this was removed.
    
    In UpdateProfileTree a conditional was added that would cause the function to stop just before updating the root node, thus the written xml was never correct and full of junk. This has been reverted.

--- Nova_UI_Core/src/HoneydConfiguration.cpp
+++ Nova_UI_Core/src/HoneydConfiguration.cpp
@@ -1630,11 +1630,6 @@ bool HoneydConfiguration::DeleteNode(std::string nodeName)
 		LOG(WARNING, "Unable to delete the Doppelganger node", "");
 		return false;
 	}
-	else if(!nodeName.compare("default"))
-	{
-		LOG(WARNING, "Unable to delete the default node", "");
-		return false;
-	}
 
 	// Make sure the node exists
 	if(!m_nodes.keyExists(nodeName))
@@ -2061,7 +2056,7 @@ bool HoneydConfiguration::UpdateProfileTree(string profileName, recursiveDirecti
 		m_profiles[profileName] = p;
 	}
 	//If the original calling profile has a parent to update
-	if((p.m_parentProfile.compare("") && p.m_parentProfile.compare("default")) && up)
+	if(p.m_parentProfile.compare("") && up)
 	{
 		//Get the parents name and create an empty ptree
 		NodeProfile parent = m_profiles[p.m_parentProfile];
