28574c93d20f3d6afdd291d8b1fe40782312d8f5
David Scott <David.Scott@DataSoft.com>
2012-07-20 23:45:40 -0700

    Qt: Fix several issues with the SuspectList, should be sorting properly and quickly
    There was errornous workflow added in that disrupted the normal process like not
    locking the table before editing it.
    
    Handled a few corner cases to assert proper insertion.
    
    Reverted a few changes that were added to compensate for inproper use of the 
    SuspectTable to restore intended functionality.

--- NovaGUI/src/novagui.h
+++ NovaGUI/src/novagui.h
@@ -57,8 +57,7 @@ public:
     Ui::NovaGUIClass ui;
 
     ///Processes the recieved suspect in the suspect table
-    	// initialization - if initializing suspects, don't overwrite existing ones and don't use the qsignal for drawing
-    void ProcessReceivedSuspect(suspectItem suspect, bool initialization);
+    void ProcessReceivedSuspect(suspectItem suspect);
 
     void SystemStatusRefresh();
 
