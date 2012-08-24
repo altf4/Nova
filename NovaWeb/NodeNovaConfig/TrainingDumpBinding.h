24b248c07a9975e16ce76f677b25ca0a129b812b
David Clark <david.clark@datasoft.com>
2012-08-24 11:49:47 -0700

    UI_WEB: Fix some references to Nova:: stuff not including the namespace

--- NovaWeb/NodeNovaConfig/TrainingDumpBinding.h
+++ NovaWeb/NodeNovaConfig/TrainingDumpBinding.h
@@ -9,7 +9,7 @@
 class TrainingDumpBinding : public node::ObjectWrap {
 public:
 	static void Init(v8::Handle<v8::Object> target);
-	TrainingDump * GetChild();
+	Nova::TrainingDump * GetChild();
 
 private:
 	TrainingDumpBinding();
@@ -17,7 +17,7 @@ private:
 
 	static v8::Handle<v8::Value> New(const v8::Arguments& args);
 
-	TrainingDump *m_db;
+	Nova::TrainingDump *m_db;
 };
 
 #endif
