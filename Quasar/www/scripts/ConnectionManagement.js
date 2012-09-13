40fa6e5d28eb1ddba817a8d9e182a3f9137466b2
awaldow <a.wal.bear@gmail.com>
2012-09-13 16:55:37 -0700

    MOTHERSHIP: Moving over connection management stuff to static js

---
+++ Quasar/www/scripts/ConnectionManagement.js
@@ -0,0 +1,72 @@
+    now.UpdateConnectionsList = function(clientId, action) {
+      var divClientList = document.getElementById('clientsList');
+      
+      switch(action)
+      {
+        case 'add':
+          var deleteMe = document.getElementById('noClients');
+          if(deleteMe != undefined)
+          {
+              divClientList.removeChild(deleteMe);
+          }
+          for(var i = 0; i < clientCount; i++)
+          {
+            if(document.getElementById('groupLabel' + i).value == clientId)
+            {
+              console.log(clientId + ' needlessly attempting to re-establish connection, doing nothing');
+              return;
+            }
+          }
+          var div = document.createElement('div');
+          div.id = clientId;
+          var check = document.createElement('input');
+          check.type = 'checkbox';
+          check.id = 'check' + (parseInt(clientCount) + 1);
+          check.name = 'check' + (parseInt(clientCount) + 1);
+          check.value = clientId;
+          check.setAttribute('onchange', 'setTarget(("check" + ' + (parseInt(clientCount) + 1) + '), clients[' + (parseInt(clientCount) + 1) + '].toString())');
+          check.setAttribute('style', 'padding-left: 50px');
+          var label = document.createElement('label');
+          label.value = clientId;
+          label.innerHTML = clientId;
+          label.setAttribute('style', 'font-weight: bold; padding-left: 50px');
+          div.appendChild(check);
+          div.appendChild(label);
+          divClientList.appendChild(div);
+          clientCount++;
+          
+          clients.push(clientId);
+          if(typeof updateGroup == 'function')
+          {
+            updateGroup('all', clients.join());
+          }
+          break;
+        case 'remove':
+          divClientList.removeChild(document.getElementById(clientId));
+          clientCount--;
+          for(var i in clients)
+          {
+            if(clients[i] == clientId)
+            {
+              clients.splice(i, 1);
+            }
+          }
+          if(clientCount == 0)
+          {
+            
+            var label = document.createElement('label');
+            label.id = 'noClients';
+            label.value = 'noClients';
+            label.innerHTML = 'There are no clients currently connected';
+            document.getElementById('clientsList').appendChild(label);
+          } 
+          if(typeof updateGroup == 'function')
+          {
+            updateGroup('all', clients.join());
+          }
+          break;
+        default:
+          console.log('UpdateConnectionsList called with invalid action, doing nothing');
+          break;
+      }
+    }
