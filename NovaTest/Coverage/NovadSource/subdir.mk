4da71f6ac281dc318a69367f1e1a58e175f63028
David Clark <david.clark@datasoft.com>
2013-01-15 14:12:42 -0700

    UTEST: Push subdir.mk file that needed updating from file relocations

--- NovaTest/Coverage/NovadSource/subdir.mk
+++ NovaTest/Coverage/NovadSource/subdir.mk
@@ -4,25 +4,40 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
+../NovadSource/ClassificationAggregator.cpp \
+../NovadSource/ClassificationEngine.cpp \
+../NovadSource/ClassificationEngineFactory.cpp \
 ../NovadSource/Control.cpp \
+../NovadSource/KnnClassification.cpp \
 ../NovadSource/Novad.cpp \
 ../NovadSource/NovadCallback.cpp \
 ../NovadSource/ProtocolHandler.cpp \
-../NovadSource/Threads.cpp 
+../NovadSource/Threads.cpp \
+../NovadSource/ThresholdTriggerClassification.cpp 
 
 OBJS += \
+./NovadSource/ClassificationAggregator.o \
+./NovadSource/ClassificationEngine.o \
+./NovadSource/ClassificationEngineFactory.o \
 ./NovadSource/Control.o \
+./NovadSource/KnnClassification.o \
 ./NovadSource/Novad.o \
 ./NovadSource/NovadCallback.o \
 ./NovadSource/ProtocolHandler.o \
-./NovadSource/Threads.o 
+./NovadSource/Threads.o \
+./NovadSource/ThresholdTriggerClassification.o 
 
 CPP_DEPS += \
+./NovadSource/ClassificationAggregator.d \
+./NovadSource/ClassificationEngine.d \
+./NovadSource/ClassificationEngineFactory.d \
 ./NovadSource/Control.d \
+./NovadSource/KnnClassification.d \
 ./NovadSource/Novad.d \
 ./NovadSource/NovadCallback.d \
 ./NovadSource/ProtocolHandler.d \
-./NovadSource/Threads.d 
+./NovadSource/Threads.d \
+./NovadSource/ThresholdTriggerClassification.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
