06b168785791da944920063452ddb663fbc11233
Rami Rashid <Rami.Rashid@datasoft.com>
2013-02-15 16:25:31 -0700

    Added a description for ThresholdTriggerClassification.h

--- Novad/src/ThresholdTriggerClassification.h
+++ Novad/src/ThresholdTriggerClassification.h
@@ -13,7 +13,8 @@
 //
 //   You should have received a copy of the GNU General Public License
 //   along with Nova.  If not, see <http://www.gnu.org/licenses/>.
-// Description : TODO: Description here
+// Description : Child class of ClassificationEngine, specifies a threshold for
+// classifying a device as an intruder
 //============================================================================
 
 #ifndef THRESHOLDTRIGGERCLASSIFICATION_H_
